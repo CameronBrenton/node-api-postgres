7c0f512004aa77370921c593fcb03aa1bb8617cb
CameronBrenton <Cameron.Brenton@keyin.com>
2021-05-31 19:09:59 -0230

    Removed bodyParser module (because it is deprecated)

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const app = express();
 const port = 3000
 
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    });
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
