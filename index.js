ac31bb972d46e19d419fe2b5a20879d85c91ae13
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-01 12:15:45 -0230

    Added some semi colons!

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/users', db.createUser);
 app.get('/users', db.getUsers);
 app.get('/users/:id', db.getUsersById);
 app.put('/users/:id', db.updateUser);
-app.delete('/users/:id', db.deleteUser)
+app.delete('/users/:id', db.deleteUser);
 
 
 app.listen(port, () => {
